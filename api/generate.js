dd836215b8b26fc481025398af13f0aa45b65ff3
Pratik Londhe <londhepratik452@gmail.com>
2024-08-03 02:15:20 +0530

    Allow CORS

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,17 @@
 const qrcode = require('qrcode');
 
 module.exports = async (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Allow specific methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
+
+    // Handle preflight requests (for OPTIONS method)
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     const { upiId, payeeName, amount, note } = req.query;
 
     if (!upiId || !payeeName) {
